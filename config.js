23eaeed35dd51c506df0993399f1feab2dfbab92
ping <18786721@qq.com>
2023-07-30 09:15:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,35 +7,35 @@ export default {
         path: "./db/faucet.db" // save request states 
     },
     project: {
-        name: "Game of NFT",
-        logo: "https://interchainnfts.dev/assets/img/banner.e3623744.png",
-        deployer: `<a href="#">Ping.pub</a>`
+        name: "Demo of Side Exchange",
+        logo: "https://side.one/favicon.ico",
+        deployer: `<a href="https://demo.side.exchange">Side Exchange</a>`
     },
     blockchains: [
         {
-            name: "gon-irishub-1",
+            name: "rigi-kent",
             endpoint: {
                 // make sure that CORS is enabled in rpc section in config.toml
                 // cors_allowed_origins = ["*"]
-                rpc_endpoint: " http://34.80.93.133:26657",
+                rpc_endpoint: " https://kent-rpc.side.exchange",
             },
             sender: {
                 mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
                 option: {
                     hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
-                    prefix: "iaa"
+                    prefix: "kent"
                 }
             },
             tx: {
                 amount: {
-                    denom: "uiris",
-                    amount: "10000000"
+                    denom: "ukent",
+                    amount: "1000000000"
                 },
                 fee: {
                     amount: [
                         {
                             amount: "5000",
-                            denom: "uiris"
+                            denom: "ukent"
                         }
                     ],
                     gas: "200000"
@@ -52,33 +52,76 @@ export default {
         {
             type: 'Ethermint',
             ids: {
-                chainId: 7000,
-                cosmosChainId: 'uptickd_7000-2',
+                chainId: 1818,
+                cosmosChainId: 'sidechain_1818-1',
             },
-            name: "uptickd_7000-2",
+            name: "Proxima",
             endpoint: {
                 // make sure that CORS is enabled in rpc section in config.toml
                 // cors_allowed_origins = ["*"]
-                rpc_endpoint: "http://52.220.252.160:26657/",
-                evm_endpoint: "http://52.220.252.160:8545/",
+                rpc_endpoint: "https://proxima-rpc.side.exchange",
+                evm_endpoint: "http://13.229.237.39:8545/",
             },
             sender: {
                 mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
                 option: {
                     hdPaths: [stringToPath("m/44'/60/0'/0/0")],
-                    prefix: "uptick"
+                    prefix: "prox"
                 }
             },
             tx: {
                 amount: {
-                    denom: "auptick",
+                    denom: "aprox",
                     amount: "5000000000000000000"
                 },
                 fee: {
                     amount: [
                         {
                             amount: "100000",
-                            denom: "auptick"
+                            denom: "aprox"
+                        }
+                    ],
+                    gas: "10000000000000"
+                },
+            },
+            limit: {
+                // how many times each wallet address is allowed in a window(24h)
+                address: 1, 
+                // how many times each ip is allowed in a window(24h),
+                // if you use proxy, double check if the req.ip is return client's ip.
+                ip: 10 
+            }
+        },
+        {
+            type: 'Ethermint',
+            ids: {
+                chainId: 1819,
+                cosmosChainId: 'sidechain_1819-1',
+            },
+            name: "Toliman",
+            endpoint: {
+                // make sure that CORS is enabled in rpc section in config.toml
+                // cors_allowed_origins = ["*"]
+                rpc_endpoint: "https://toliman-rpc.side.exchange",
+                evm_endpoint: "http://52.77.209.10:8545/",
+            },
+            sender: {
+                mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
+                option: {
+                    hdPaths: [stringToPath("m/44'/60/0'/0/0")],
+                    prefix: "toli"
+                }
+            },
+            tx: {
+                amount: {
+                    denom: "atoli",
+                    amount: "5000000000000000000"
+                },
+                fee: {
+                    amount: [
+                        {
+                            amount: "100000",
+                            denom: "atoli"
                         }
                     ],
                     gas: "10000000000000"

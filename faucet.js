d9aa8e8779cbfb74c5ac2d5615722fc955c281fa
liangping <18786721@qq.com>
2023-07-31 10:42:39 +0800

    update balance query

--- faucet.js
+++ faucet.js
@@ -3,6 +3,7 @@ import * as path from 'path'
 
 import { Wallet } from '@ethersproject/wallet'
 import { pathToString } from '@cosmjs/crypto';
+import { fromBech32, toBech32, fromHex} from '@cosmjs/encoding'
 
 import { ethers } from 'ethers';
 import { bech32 } from 'bech32';
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
 
 app.get('/config.json', async (req, res) => {
   const sample = {}
-  for(let i =0; i < conf.blockchains.length; i++) {
+  for (let i = 0; i < conf.blockchains.length; i++) {
     const chainConf = conf.blockchains[i]
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
     const [firstAccount] = await wallet.getAccounts();
@@ -43,65 +44,58 @@ app.get('/config.json', async (req, res) => {
 })
 
 app.get('/balance/:chain', async (req, res) => {
-  const { chain }= req.params
+  const { chain } = req.params
 
   let balance = {}
 
-  try{
+  try {
     const chainConf = conf.blockchains.find(x => x.name === chain)
-    if(chainConf) {
-      if(chainConf.type === 'Ethermint') {
+    if (chainConf) {
+      if (chainConf.type === 'Ethermint') {
         // const ethProvider = new ethers.providers.JsonRpcProvider(chainConf.endpoint.evm_endpoint);
-        // const wallet = Wallet.fromMnemonic(chainConf.sender.mnemonic).connect(ethProvider);
-        // await wallet.getBalance().then(ethBlance => {
-        //   balance = {
-        //     denom:chainConf.tx.amount.denom,
-        //     amount:ethBlance.toString()
-        //   }
-        // }).catch(e => console.error(e))
+        const wallet2 = Wallet.fromMnemonic(chainConf.sender.mnemonic, pathToString(chainConf.sender.option.hdPaths[0]));
         const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
 
         const client = await SigningStargateClient.connect(rpcEndpoint);
-        const [firstAccount] = await wallet.getAccounts();
-        await client.getBalance(firstAccount.address, chainConf.tx.amount.denom).then(x => {
+        const faucetAddress = toBech32(chainConf.sender.option.prefix, fromHex(wallet2.address.replace('0x', '')));
+        await client.getBalance(faucetAddress, chainConf.tx.amount.denom).then(x => {
           return balance = x
         }).catch(e => console.error(e));
       }
 
-      }else{
-        const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
-        const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
-        const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
-        const [firstAccount] = await wallet.getAccounts();
-        await client.getBalance(firstAccount.address, chainConf.tx.amount.denom).then(x => {
-          return balance = x
-        }).catch(e => console.error(e));
-      }
+    } else {
+      const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
+      const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
+      const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
+      const [firstAccount] = await wallet.getAccounts();
+      await client.getBalance(firstAccount.address, chainConf.tx.amount.denom).then(x => {
+        return balance = x
+      }).catch(e => console.error(e));
     }
-  } catch(err) {
+  } catch (err) {
     console.log(err)
   }
   res.send(balance);
 })
 
 app.get('/send/:chain/:address', async (req, res) => {
-  const {chain, address} = req.params;
+  const { chain, address } = req.params;
   const ip = req.headers['x-real-ip'] || req.headers['X-Real-IP'] || req.headers['X-Forwarded-For'] || req.ip
   console.log('request tokens to ', address, ip)
-  if (chain || address ) {
+  if (chain || address) {
     try {
       const chainConf = conf.blockchains.find(x => x.name === chain)
       if (chainConf && (address.startsWith(chainConf.sender.option.prefix) || address.startsWith('0x'))) {
-        if( await checker.checkAddress(address, chain) && await checker.checkIp(`${chain}${ip}`, chain) ) {
+        if (await checker.checkAddress(address, chain) && await checker.checkIp(`${chain}${ip}`, chain)) {
           checker.update(`${chain}${ip}`) // get ::1 on localhost
           sendTx(address, chain).then(ret => {
 
             checker.update(address)
             res.send({ result: ret })
           }).catch(err => {
-            res.send({ result: `err: ${err}`})
+            res.send({ result: `err: ${err}` })
           });
-        }else {
+        } else {
           res.send({ result: "You requested too often" })
         }
       } else {
@@ -124,8 +118,8 @@ app.listen(conf.port, () => {
 
 async function sendCosmosTx(recipient, chain) {
   // const mnemonic = "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put";
-  const chainConf = conf.blockchains.find(x => x.name === chain) 
-  if(chainConf) {
+  const chainConf = conf.blockchains.find(x => x.name === chain)
+  if (chainConf) {
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
     const [firstAccount] = await wallet.getAccounts();
 
@@ -133,50 +127,49 @@ async function sendCosmosTx(recipient, chain) {
 
     const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
     const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
-    client.getBalance
 
     // const recipient = "cosmos1xv9tklw7d82sezh9haa573wufgy59vmwe6xxe5";
     const amount = chainConf.tx.amount;
     const fee = chainConf.tx.fee;
-    return client.sendTokens(firstAccount.address, recipient, [amount], fee);
+    return client.sendTokens(firstAccount.address, recipient, amount, fee);
   }
   throw new Error(`Blockchain Config [${chain}] not found`)
 }
 
 async function sendEvmosTx(recipient, chain) {
 
-  try{
-    const chainConf = conf.blockchains.find(x => x.name === chain) 
+  try {
+    const chainConf = conf.blockchains.find(x => x.name === chain)
     // const ethProvider = new ethers.providers.JsonRpcProvider(chainConf.endpoint.evm_endpoint);
 
     const wallet = Wallet.fromMnemonic(chainConf.sender.mnemonic); // .connect(ethProvider);
 
-    let evmAddress =  recipient;
-    if(recipient && !recipient.startsWith('0x')) {
+    let evmAddress = recipient;
+    if (recipient && !recipient.startsWith('0x')) {
       let decode = bech32.decode(recipient);
       let array = bech32.fromWords(decode.words);
-      evmAddress =  "0x" + toHexString(array);
+      evmAddress = "0x" + toHexString(array);
     }
 
     let result = await wallet.sendTransaction(
-        { 
-          from:wallet.address,
-          to:evmAddress,
-          value:chainConf.tx.amount.amount
-        }
-      );
-   
+      {
+        from: wallet.address,
+        to: evmAddress,
+        value: chainConf.tx.amount.amount
+      }
+    );
+
     let repTx = {
-      "code":0,
-      "nonce":result["nonce"],
-      "value":result["value"].toString(),
-      "hash":result["hash"]
+      "code": 0,
+      "nonce": result["nonce"],
+      "value": result["value"].toString(),
+      "hash": result["hash"]
     };
 
-    console.log("xxl result : ",repTx);
+    console.log("Token sent : ", repTx);
     return repTx;
-  }catch(e){
-    console.log("xxl e ",e);
+  } catch (e) {
+    console.log("Sending tx error: ", e);
     return e;
   }
 
@@ -184,23 +177,14 @@ async function sendEvmosTx(recipient, chain) {
 
 function toHexString(bytes) {
   return bytes.reduce(
-      (str, byte) => str + byte.toString(16).padStart(2, '0'), 
-      '');
+    (str, byte) => str + byte.toString(16).padStart(2, '0'),
+    '');
 }
 
 async function sendTx(recipient, chain) {
-  const chainConf = conf.blockchains.find(x => x.name === chain) 
-  if(chainConf.type === 'Ethermint') {
+  const chainConf = conf.blockchains.find(x => x.name === chain)
+  if (chainConf.type === 'Ethermint') {
     return sendEvmosTx(recipient, chain)
   }
   return sendCosmosTx(recipient, chain)
-}
-
-// write a function to send evmos transaction
-async function sendEvmosTx2(recipient, chain) {
-
-  // use evmosjs to send transaction
-  const chainConf = conf.blockchains.find(x => x.name === chain) 
-  // create a wallet instance
-  const wallet = Wallet.fromMnemonic(chainConf.sender.mnemonic).connect(chainConf.endpoint.evm_endpoint);
 }
